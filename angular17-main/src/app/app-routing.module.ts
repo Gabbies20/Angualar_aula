7a3a1db44b52f2d220e6f975829c7b3220aac63a
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2024-02-05 00:30:44 +0100

    He creado el componente noticias y header

--- angular17-main/src/app/app-routing.module.ts
+++ angular17-main/src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmpleadoComponent} from './empleado/empleado.component'
 import { CochesComponent} from './coches/coches.component'
 import { HomeComponent } from './home/home.component';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 //A continuacion definimos las rutas:
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'emleado', component: EmpleadoComponent},
-  { path: 'coche', component: CochesComponent},
-  { path: '**', component: HomeComponent},
+  { path: 'empleado', component: EmpleadoComponent},
+  { path: 'coches', component: CochesComponent},
+  { path: 'articulos', component: ArticulosComponent},
+  { path: '**', redirectTo: 'home'},
+
 ];
 
 //Linea importante:
-//
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- angular17-main/src/app/app.module.ts
+++ angular17-main/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { EmpleadoComponent } from './empleado/empleado.component';
 import { CochesComponent } from './coches/coches.component';
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule } from '@angular/common/http';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoComponent,
     CochesComponent,
-    HomeComponent
+    HomeComponent,
+    ArticulosComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
